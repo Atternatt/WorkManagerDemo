67d595c0b53f8c84cd475cdc435fd143993ee3c7
Marc Moreno <ignis.fatue@gmail.com>
2019-01-08 11:55:35 +0100

    updated application to implement DaggerApplication

--- app/src/main/java/com/bakeronline/app/main/BakerApplication.kt
+++ app/src/main/java/com/bakeronline/app/main/BakerApplication.kt
@@ -1,30 +1,18 @@
 package com.bakeronline.app.main
 
-import android.app.Activity
-import android.app.Application
-import android.app.Service
 import android.os.StrictMode
 import com.bakeronline.app.BuildConfig
 import com.bakeronline.app.di.initInjection
 import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import dagger.android.HasServiceInjector
-import javax.inject.Inject
+import dagger.android.DaggerApplication
 
 
-class BakerApplication : Application(), HasActivityInjector, HasServiceInjector {
+class BakerApplication : DaggerApplication() {
 
-    @Inject
-    lateinit var serviceInjector: DispatchingAndroidInjector<Service>
-
-    override fun serviceInjector(): AndroidInjector<Service> = serviceInjector
-
-    @Inject
-    lateinit var injector: DispatchingAndroidInjector<Activity>
-
-    override fun activityInjector() = injector
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate() {
         super.onCreate()
